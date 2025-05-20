4bb6538c61ff714d6658478267a55a6d7befd89b
Pedro Alves Estevam Magaieski <46140459877@>
2025-05-20 10:12:33 -0300

    valid

--- src/Paginas/Cadastro.jsx
+++ src/Paginas/Cadastro.jsx
@@ -2,6 +2,7 @@ import estilo from './Cadastro.module.css'
 import axios from 'axios';
 import React, {useState} from 'react';
 
+
 const API_URL = 'http://127.0.0.1:8000'
 export function Cadastro(){
         const [formData,setFormData] = useState({
@@ -32,34 +33,50 @@ export function Cadastro(){
         <main className={estilo.container}>
                 <form onSubmit={handleSubmit} className={estilo.formFlex}>
                     <h2 className={estilo.titulo}>Cadastre-se</h2>
+                    <label className={estilo.label}>Nome
+
+                    </label>
                     <input className={estilo.input} type="text" name='nome' placeholder='Nome de usuario' 
-                    defaultValue={formData.nome} 
-                    onChange={handleChange} />
+                        defaultValue={formData.nome} 
+                        onChange={handleChange} />
+
                     <div className={estilo.formGrid}>
-                        <input className={estilo.input} type="text" name='ni' placeholder='Numero de identificação'
-                        defaultValue={formData.ni}
-                        onChange={handleChange}/>
-                        <input className={estilo.input} type="text" name='telefone' placeholder='Telefone'
-                        defaultValue={formData.telefone}
-                        onChange={handleChange}/>
-                        <label htmlFor='Data de nascimento'>Data Nascimento
+                        
+                        <label className={estilo.label}>Numero de Identificação
+                            <input className={estilo.input} type="text" name='ni' placeholder='Numero de identificação'
+                            defaultValue={formData.ni}
+                            onChange={handleChange}/>
+                        </label>
+
+                        <label className={estilo.label}>Telefone 
+                            <input className={estilo.input} type="text" name='telefone' placeholder='Telefone'
+                            defaultValue={formData.telefone}
+                            onChange={handleChange}/>
+                        </label>
+
+                        <label className={estilo.label}>Data Nascimento
                             <input className={estilo.input} type="date" name='data_nascimento' placeholder='Data de nascimento' 
                             defaultValue={formData.data_nascimento}
                             onChange={handleChange}/>
                         </label>
-                        <label htmlFor='Data de contratação'>Data de contratação
+                        <label >Data de contratação
                             <input className={estilo.input} type="date" name='data_contratação' placeholder='Data de contratação'
                             defaultValue={formData.data_contratação} 
                             onChange={handleChange}/>
                         </label>
-                        <input className={estilo.input} type="password" name='password' placeholder='Senha'
-                            defaultValue={formData.password}
-                            onChange={handleChange} />
-                        <input className={estilo.input} type="password" name='password2' placeholder='Confirme sua Senha'
-                            defaultValue={formData.password2} 
-                            onChange={handleChange} />
+                        <label >Senha 
+                            <input className={estilo.input} type="password" name='password' placeholder='Senha'
+                                defaultValue={formData.password}
+                                onChange={handleChange} />
+                        </label>
+                        <label >Confirme sua senha
+                            <input className={estilo.input} type="password" name='password2' placeholder='Confirme sua Senha'
+                                defaultValue={formData.password2} 
+                                onChange={handleChange} />
+                        </label>
+
                         
-                        <label>
+                        <label className={estilo.label}>
                             Usuario comum
                             <input 
                             type="radio" 
@@ -68,7 +85,7 @@ export function Cadastro(){
                             checked={formData.categoria  === "C"}
                             onChange={handleChange}/>
                         </label>
-                        <label>
+                        <label className={estilo.label}>
                             Usuario gestor
                             <input 
                             type="radio"
--- src/Paginas/Login.jsx
+++ src/Paginas/Login.jsx
@@ -1,38 +1,87 @@
 import axios from 'axios';
-import React, {useState} from 'react';
-import estilo from './Login.module.css'
+import React, {use, useState} from 'react';
+import estilo from './Login.module.css';
+import {useForm} from 'react-hook-form';
+import {z} from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {data, useNavigate} from 'react-router-dom'
 
 const API_URL = 'http://127.0.0.1:8000'
 
+const schemaLogin = z.object({
+    username: z.string()
+        .min(1,'Informe seu usuário')
+        .max(15, 'Informe até 25 caracteres'),
+    password: z.string()
+        .min(1,'Informe seu usuário')
+        .max(15, 'Informe até 25 caracteres'),
+})
+
 export function Login(){
-    
-    const [formData,setFormData] = useState({
-        username: '',
-        password: ''
-    });
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const navigate = useNavigate();
+
+    const{
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({
+        resolver: zodResolver(schemaLogin)
+    })
+
+    async function obterDados(formData){
+        console.log(`dados: ${formData}`)
+
         try{
-            const response = await axios.post(`${API_URL}/api/token/`,formData)
-            console.log('Dados Enviados sucesso: ', response.data)
-        } catch (error){
-            console.error('Erro ao enviar dados: ',error)
+            const response = await axios.post(`${API_URL}/api/token/`,{
+                username: data.username,
+                password: data.password
+            });
+            const {acess, refresh, user} = response.data;
+            localStorage.getItem('access_token', acess);
+            localStorage.setItem('refresh_token', refresh);
+            localStorage.setItem('categoria', user.categoria);
+            localStorage.setItem('user_id', user.id);
+            localStorage.setItem('username',username)
+
+            console.log('login realizado')
+
+            navigate('/inicial');
+        }catch(error){
+            console.error('erro no login', error);
+            alert('credenciais inválidas')
         }
-    };
+    }
+
+    // const [formData,setFormData] = useState({
+    //     username: '',
+    //     password: ''
+    // });
+    // const handleChange = (e) => {
+    //     setFormData({ ...formData, [e.target.name]: e.target.value });
+    // };
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     try{
+    //         const response = await axios.post(`${API_URL}/api/token/`,formData)
+    //         console.log('Dados Enviados sucesso: ', response.data)
+    //     } catch (error){
+    //         console.error('Erro ao enviar dados: ',error)
+    //     }
+    // };
+
     return(
         <main className={estilo.container}>
             <form onSubmit={handleSubmit} className={estilo.formFlex}>
                 <h2 className={estilo.titulo}>Entrar</h2>
-                <input className={estilo.input} name='username' type="text" 
-                value={formData.username} 
-                onChange={handleChange} placeholder='Nome de usuario'/>
-                <input className={estilo.input} name='password' type="password" 
-                value={formData.password}
-                onChange={handleChange} placeholder='Senha'/>
-                <input className={estilo.submit} type="submit"/>
+                
+                <label>Nome</label>
+                <input className={estilo.input} name='username' type="text" {...register('username')} placeholder='Nome de usuario'/>
+                {errors.username && <p>{errors.username.message}</p>}
+                
+                <label>Senha</label>
+                <input className={estilo.input} {...register('username')} name='password' type="password" placeholder='Senha'/>
+                {errors.password && <p>{errors.password.message}</p>}
+                <button className={estilo.submit} type="submit">Enviar</button>
             </form>
         </main>
     )
